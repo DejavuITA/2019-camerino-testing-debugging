d67cfe6fe181009549bdd7e53dea053725eac8f0
ASPP Student <student@localhost>
2019-09-03 12:23:18 +0200

    changed the test with a given, when, then pattern

--- hands_on/pyanno_voting/pyanno/tests/test_voting.py
+++ hands_on/pyanno_voting/pyanno/tests/test_voting.py
@@ -5,6 +5,7 @@ from pyanno.voting import MISSING_VALUE as MV
 
 
 def test_labels_count():
+    #Given
     annotations = [
         [1,  2, MV, MV],
         [MV, MV,  3,  3],
@@ -13,7 +14,11 @@ def test_labels_count():
     ]
     nclasses = 5
     expected = [0, 3, 1, 3, 0]
+
+    #When
     result = voting.labels_count(annotations, nclasses)
+
+    #Then
     assert result == expected
 
 
@@ -44,6 +49,7 @@ def test_majority_vote_empty_item():
 
 
 def test_label_frequency():
+    #Given
     annotations = np.array(
         [[1, 2, 3],
          [-1, -1, -1],
@@ -51,5 +57,9 @@ def test_label_frequency():
     )
     n_classes = 4
     expected = np.array([2/6, 3/6, 1/6, 0/6])
+
+    #When
     result = voting.labels_frequency(annotations, n_classes)
+
+    #Then
     assert np.allclose(result, expected, rtol = 1e-6)
